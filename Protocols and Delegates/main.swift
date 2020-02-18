b6ebf3dd7f6eebf6b9a2835ae6a44083ba5271a3
agil <antonio_g_s@hotmail.com>
2020-02-18 22:14:55 +0100

    Initial Commit

---
+++ Protocols and Delegates/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Protocols and Delegates
+//
+//  Created by Antonio Gil Sánchez on 18/02/2020.
+//  Copyright © 2020 Antonio Gil Sánchez. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
