6d4b736098dab702843b738bb1170049760f57da
agil <antonio_g_s@hotmail.com>
2020-03-12 23:50:36 +0100

    Final

--- Protocols and Delegates/main.swift
+++ Protocols and Delegates/main.swift
@@ -1,12 +1,63 @@
-//
-//  main.swift
-//  Protocols and Delegates
-//
-//  Created by Antonio Gil Sánchez on 18/02/2020.
-//  Copyright © 2020 Antonio Gil Sánchez. All rights reserved.
-//
 
-import Foundation
+protocol AdvanceLifeSupport {
+    func performRCP()
+}
 
-print("Hello, World!")
+class EmergencyCallHandler {
+    var delegate : AdvanceLifeSupport?
+    
+    func assessSituation() {
+        print("Qué ha pasado?")
+    }
+    
+    func medicalEmergency() {
+        delegate?.performRCP()
+    }
+}
 
+struct Paramedic: AdvanceLifeSupport {
+    init(handler: EmergencyCallHandler) {
+        handler.delegate = self
+    }
+    
+    func performRCP() {
+        print("El paramédico realiza 30 compresiones por segundo")
+    }
+}
+
+class Doctor: AdvanceLifeSupport {
+    init(handler: EmergencyCallHandler) {
+        handler.delegate = self
+    }
+    
+    func performRCP() {
+        print("El doctor realiza 30 compresiones por segundo")
+    }
+    
+    func useStethescope() {
+        print("Escuchando el corazón")
+    }
+}
+
+class Surgeon: Doctor {
+    override func performRCP() {
+        super.performRCP()
+        print("Canta staging alive de los BeeGees")
+    }
+    
+    func useElectricDrill() {
+        print("Whirr....")
+    }
+}
+
+//let emilio = EmergencyCallHandler()
+//let pete = Paramedic(handler: emilio)
+//
+//emilio.assessSituation()
+//emilio.medicalEmergency()
+
+let emilio = EmergencyCallHandler()
+let angela = Surgeon(handler: emilio)
+
+emilio.assessSituation()
+emilio.medicalEmergency()
